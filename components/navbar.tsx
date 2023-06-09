0f1becba45322039ff994cd518d1644a73d2cf44
Bradley Gulli <bradleyjgulli@gmail.com>
2023-06-09 00:30:17 -0700

    Initial website structure

---
+++ components/navbar.tsx
@@ -0,0 +1,23 @@
+import Link from "next/link";
+
+const Navbar = () => {
+  return (
+    <nav className="pb-4 top-0 justify-center items-center flex">
+      <ul className="flex text-gray-900 font-mono space-x-3 text-3xl font-semibold">
+        <li className="font-noto transition ease-in-out hover:-translate-y-1 hover:scale-110 hover:text-orange-300 duration-300">
+          <Link href="/">Home</Link>
+        </li>
+        <div> | </div>
+        <li  className="font-noto transition ease-in-out hover:-translate-y-1 hover:scale-110 hover:text-orange-300 duration-300">
+          <Link href="/resume">Resume</Link>
+        </li>
+        <div> | </div>
+        <li  className="font-noto transition ease-in-out hover:-translate-y-1 hover:scale-110 hover:text-orange-300 duration-300">
+          <Link href="/projects">Projects</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;

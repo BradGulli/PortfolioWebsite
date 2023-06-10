96d7d0f640ded8ed0cda4715f27194dd6d68825f
Bradley Gulli <bradleyjgulli@gmail.com>
2023-06-10 00:52:45 -0700

    make nav smaller on small screens

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 const Navbar = () => {
   return (
     <nav className="pb-4 top-0 justify-center items-center flex">
-      <ul className="flex text-gray-900 font-mono space-x-3 text-3xl font-semibold">
+      <ul className="flex text-gray-900 font-mono space-x-3 text-s md:text-3xl font-semibold">
         <li className="font-noto transition ease-in-out hover:-translate-y-1 hover:scale-110 hover:text-orange-300 duration-300">
           <Link href="/">Home</Link>
         </li>
